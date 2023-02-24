8d31b57558744cec7a147d39cd483ddd92cc9a51
Boris Shirokikh <bg.dustpelt@gmail.com>
2023-02-24 13:20:21 +0300

    v0.3.3: add nsclc and medseg9; add notebook for brief testing

--- oodbench/datasets/ct/__init__.py
+++ oodbench/datasets/ct/__init__.py
@@ -2,3 +2,5 @@ from .cancer500 import Cancer500
 from .ct_ich import CT_ICH
 from .lidc import LIDC
 from .lits import LiTS
+from .medseg9 import Medseg9
+from .nsclc import NSCLC
---
+++ oodbench/datasets/ct/medseg9.py
@@ -0,0 +1,45 @@
+import numpy as np
+from amid import CacheToDisk, CacheColumns
+from amid.medseg9 import Medseg9
+from connectome import Transform, Apply, chained
+
+from ...const import CT_COMMON_SPACING
+from ..transforms import Rescale, ScaleIntensityCT, AddShape, TrainTestSplit
+
+
+__all__ = ['Medseg9', ]
+
+
+class RenameFieldsMedseg9(Transform):
+    __exclude__ = ('affine', 'voxel_spacing', )
+
+    def mask(image):
+        return np.zeros_like(image, dtype=bool)
+
+
+class CanonicalOrientation(Transform):
+    __inherit__ = True
+    _flip_x: bool = True
+
+    def image(image, _flip_x):
+        return np.transpose(image, (1, 0, 2))
+
+    def mask(mask, _flip_x):
+        return np.transpose(mask, (1, 0, 2))
+
+    def spacing(spacing):
+        return tuple(np.array(spacing)[[1, 0, 2]].tolist())
+
+
+Medseg9 = chained(
+    TrainTestSplit(),
+    RenameFieldsMedseg9(),
+    CanonicalOrientation(),
+    Rescale(new_spacing=CT_COMMON_SPACING),
+    ScaleIntensityCT(),
+    AddShape(),
+    CacheToDisk(('ids', 'train_ids', 'test_ids', )),
+    CacheColumns(('shape', 'spacing', )),
+    Apply(image=np.float16, mask=np.bool_),
+    Apply(image=np.float32, mask=np.float32)
+)(Medseg9)
