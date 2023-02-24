7931c2fead6b23ba3e9cb3bfa6ef623af9b77e5a
Boris Shirokikh <bg.dustpelt@gmail.com>
2023-02-24 11:48:25 +0300

    v0.3.1: added lits (tested)

--- oodbench/datasets/ct/__init__.py
+++ oodbench/datasets/ct/__init__.py
@@ -1,3 +1,4 @@
 from .cancer500 import Cancer500
 from .ct_ich import CT_ICH
 from .lidc import LIDC
+from .lits import LiTS
---
+++ oodbench/datasets/ct/lits.py
@@ -0,0 +1,31 @@
+import numpy as np
+from amid import CacheToDisk, CacheColumns
+from amid.lits import LiTS, CanonicalCTOrientation, Rescale
+from connectome import Transform, Apply, chained
+
+from ...const import CT_COMMON_SPACING
+from ..transforms import ScaleIntensityCT, AddShape, TrainTestSplit
+
+
+__all__ = ['LiTS', ]
+
+
+class ChangeFieldsLiTS(Transform):
+    __inherit__ = True
+
+    def mask(image):
+        return np.zeros_like(image, dtype=bool)
+
+
+LiTS = chained(
+    TrainTestSplit(),
+    CanonicalCTOrientation(),
+    Rescale(new_spacing=CT_COMMON_SPACING),
+    ChangeFieldsLiTS(),
+    ScaleIntensityCT(),
+    AddShape(),
+    CacheToDisk(('ids', 'train_ids', 'test_ids', )),
+    CacheColumns(('shape', 'spacing', )),
+    Apply(image=np.float16, mask=np.bool_),
+    Apply(image=np.float32, mask=np.float32)
+)(LiTS)
